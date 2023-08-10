487856fe20ab82a567d7074b1dbaba0bd36739e5
harol <harol182014@gmail.com>
2023-08-10 11:19:03 -0500

    nv

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,10 @@ import { Router } from "express";
 const router = Router()
 
 router.get('/',(req, res) => res.render('index',{ title:'Bienvenido a Nodejs - WebApp'}))
-router.get('/about',(req, res) => res.render('about',{ title:'About me'}))
+router.get('/somos',(req, res) => res.render('somos',{ title:'Quièn somos'}))
+router.get('/servicios',(req, res) => res.render('servicios',{ title:'Servicios'}))
+router.get('/portafolio',(req, res) => res.render('portafolio',{ title:'Portafolio'}))
+router.get('/testimonio',(req, res) => res.render('testimonio',{ title:'Testimonio'}))
 router.get('/contact',(req, res) => res.render('contact',{ title:'Contact me'}))
 
 export default router
