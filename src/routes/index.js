9a4c93b4429a2c9da4321b8d929d2a46252f039e
harw18 <harol182014@gmail.com>
2023-08-11 17:30:34 -0500

    ultimotrabajo

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = Router()
 router.get('/',(req, res) => res.render('index',{ title:'Bienvenido a Nodejs - WebApp'}))
 router.get('/somos',(req, res) => res.render('somos',{ title:'Quièn somos'}))
 router.get('/servicios',(req, res) => res.render('servicios',{ title:'Servicios'}))
-router.get('/portafolio',(req, res) => res.render('portafolio',{ title:'Portafolio'}))
+router.get('/porfolio',(req, res) => res.render('porfolio',{ title:'Porfolio'}))
 router.get('/testimonio',(req, res) => res.render('testimonio',{ title:'Testimonio'}))
 router.get('/contact',(req, res) => res.render('contact',{ title:'Contact me'}))
 
